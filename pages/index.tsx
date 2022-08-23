ca0b304da989ac8c30d3c5ccd69e3c0abf6ceaf4
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2022-08-23 22:21:30 +0530

    :sparkles: update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import { Flex, Image, Heading, Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Head from "next/head";
 
@@ -7,26 +6,9 @@ const Home: NextPage = () => {
 		<>
 			<Head>
 				<title>0xarihant</title>
-				<meta
-					name="description"
-					content="Portfolio site built with TypeScript + Next.js"
-				/>
-				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<Flex justify="center" align="center" direction="column">
-				<Image
-					src="https://pbs.twimg.com/profile_images/1537100684570423296/FXnUSb12_400x400.jpg"
-					alt="A headshot of Arihant"
-					boxSize="10em"
-					borderRadius="full"
-					m="2"
-				/>
-				<Heading as="h1">Hi, I&apos;m Arihant 👋, a developer</Heading>
-				<Text></Text>
-			</Flex>
 		</>
 	);
 };
 
 export default Home;
-
