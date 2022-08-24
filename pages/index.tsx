9c23b23babc94b2b860c36f03142e7bd460e0217
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2022-08-24 11:08:40 +0530

    :art:

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import { Box, Flex, IconButton, Text } from "@chakra-ui/react";
+import { Box, Flex, IconButton, Text, Heading } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { FaTwitter } from "react-icons/fa";
+import { FaTwitter, FaLinkedin } from "react-icons/fa";
+import { BsGithub } from "react-icons/bs";
 import { CustomLink } from "../components/CustomLink";
 
 const Home: NextPage = () => {
@@ -12,7 +13,9 @@ const Home: NextPage = () => {
 			</Head>
 			<Box p="4">
 				<Flex alignItems="center" justifyContent="space-between" mx="auto">
-					<Text>Arihant Bansal</Text>
+					<Heading as="h1" size="lg">
+						Arihant Bansal
+					</Heading>
 					<Flex>
 						<CustomLink href="https://twitter.com/_arihantbansal">
 							<IconButton
@@ -23,8 +26,39 @@ const Home: NextPage = () => {
 								_hover={{ color: "#1DA1F2" }}
 							/>
 						</CustomLink>
+						<CustomLink href="https://github.com/arihantbansal">
+							<IconButton
+								aria-label="GitHub"
+								icon={<BsGithub />}
+								variant="ghost"
+								fontSize="20px"
+								_hover={{}}
+							/>
+						</CustomLink>
+						<CustomLink href="https://linkedin.com/in/arihantbansal">
+							<IconButton
+								aria-label="LinkedIn"
+								icon={<FaLinkedin />}
+								variant="ghost"
+								fontSize="20px"
+								_hover={{ color: "#0072b1" }}
+							/>
+						</CustomLink>
 					</Flex>
 				</Flex>
+
+				<Box>
+					<Text>
+						web3 dev. serial builder. passionate about
+						<Text
+							as="span"
+							bgGradient="linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)"
+							bgClip="text">
+							privacy
+						</Text>
+						, data sovereignty, and open source.
+					</Text>
+				</Box>
 			</Box>
 		</>
 	);
