bd8ec4d215812513102802a13a130b287c0b175d
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2022-08-24 11:07:44 +0530

    :sparkles:

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
+import { Box, Flex, IconButton, Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { FaTwitter } from "react-icons/fa";
+import { CustomLink } from "../components/CustomLink";
 
 const Home: NextPage = () => {
 	return (
@@ -7,6 +10,22 @@ const Home: NextPage = () => {
 			<Head>
 				<title>0xarihant</title>
 			</Head>
+			<Box p="4">
+				<Flex alignItems="center" justifyContent="space-between" mx="auto">
+					<Text>Arihant Bansal</Text>
+					<Flex>
+						<CustomLink href="https://twitter.com/_arihantbansal">
+							<IconButton
+								aria-label="Twitter"
+								icon={<FaTwitter />}
+								variant="ghost"
+								fontSize="20px"
+								_hover={{ color: "#1DA1F2" }}
+							/>
+						</CustomLink>
+					</Flex>
+				</Flex>
+			</Box>
 		</>
 	);
 };
