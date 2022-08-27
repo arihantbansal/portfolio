167fe2c2b27ae739ccb62abc535bb594e4659e0e
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2022-08-27 16:48:58 +0530

    :sparkles: update content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider, Flex } from "@chakra-ui/react";
 import "../styles/globals.css";
+import "@fontsource/ibm-plex-sans";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider>
-			<Flex minH="100vh" direction="column">
+			<Flex minH="100vh" direction="column" bgColor="000">
 				<Component {...pageProps} />
 			</Flex>
 		</ChakraProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,62 +1,35 @@
-import { Box, Flex, IconButton, Text, Heading } from "@chakra-ui/react";
+import { Box, Flex, Text, Heading } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { FaTwitter, FaLinkedin } from "react-icons/fa";
-import { BsGithub } from "react-icons/bs";
 import { CustomLink } from "../components/CustomLink";
 
 const Home: NextPage = () => {
 	return (
 		<>
 			<Head>
-				<title>0xarihant</title>
+				<title>Arihant</title>
 			</Head>
 			<Box p="4">
 				<Flex alignItems="center" justifyContent="space-between" mx="auto">
 					<Heading as="h1" size="lg">
 						Arihant Bansal
 					</Heading>
-					<Flex>
-						<CustomLink href="https://twitter.com/_arihantbansal">
-							<IconButton
-								aria-label="Twitter"
-								icon={<FaTwitter />}
-								variant="ghost"
-								fontSize="20px"
-								_hover={{ color: "#1DA1F2" }}
-							/>
-						</CustomLink>
-						<CustomLink href="https://github.com/arihantbansal">
-							<IconButton
-								aria-label="GitHub"
-								icon={<BsGithub />}
-								variant="ghost"
-								fontSize="20px"
-								_hover={{}}
-							/>
-						</CustomLink>
-						<CustomLink href="https://linkedin.com/in/arihantbansal">
-							<IconButton
-								aria-label="LinkedIn"
-								icon={<FaLinkedin />}
-								variant="ghost"
-								fontSize="20px"
-								_hover={{ color: "#0072b1" }}
-							/>
-						</CustomLink>
-					</Flex>
 				</Flex>
-
-				<Box>
-					<Text>
-						web3 dev. serial builder. passionate about
-						<Text
-							as="span"
-							bgGradient="linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)"
-							bgClip="text">
+				<Box my="16">
+					<Text as="h2" fontSize="3xl" fontWeight="normal">
+						nineteen. web3 dev. serial builder. <br />
+						passionate about{" "}
+						<Text as="span" bgGradient="linear-gradient(to right, #56ab2f, #a8e063)" bgClip="text">
 							privacy
 						</Text>
-						, data sovereignty, and open source.
+						,{" "}
+						<Text as="span" bgGradient="linear-gradient(to right, #00b4db, #0083b0)" bgClip="text">
+							data sovereignty
+						</Text>
+						, and{" "}
+						<Text as="span" bgGradient="linear-gradient(to right, #00b4db, #0083b0)" bgClip="text">
+							open source software.
+						</Text>
 					</Text>
 				</Box>
 			</Box>
