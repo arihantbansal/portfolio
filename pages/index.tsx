231946572eecb56d1631c57e4b9cd77a2981348d
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2023-03-19 15:36:01 +0530

    :bug: fix document head

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,33 +9,9 @@ const Home: NextPage = () => {
 		<>
 			<Head>
 				<title>Arihant</title>
+				<meta property="og:title" content="Arihant" key="title" />
 			</Head>
 			<Box p="12">
-				<Flex alignItems="center" justifyContent="space-between" mx="auto">
-					<Heading as="h1" size="lg">
-						Arihant Bansal
-					</Heading>
-				</Flex>
-				<Box my="16">
-					<Text as="h2" fontSize="3xl" fontWeight="normal">
-						buildoor. engineering undergrad at bits pilani, india.
-					</Text>
-					<br />
-					<Text as="h2" fontSize="3xl" fontWeight="normal">
-						passionate about{" "}
-						<Text as="span" bgGradient="linear-gradient(to right, #56ab2f, #a8e063)" bgClip="text">
-							privacy
-						</Text>
-						,{" "}
-						<Text as="span" bgGradient="linear-gradient(to right, #00b4db, #0083b0)" bgClip="text">
-							data sovereignty
-						</Text>
-						, and{" "}
-						<Text as="span" bgGradient="linear-gradient(to right, #00b4db, #0083b0)" bgClip="text">
-							open source software.
-						</Text>
-					</Text>
-				</Box>
 				<Footer />
 			</Box>
 		</>
