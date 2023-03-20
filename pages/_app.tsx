e1f2e892149fd30ab431e51d097d71b3a291a963
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2023-03-20 12:31:59 +0530

    :lipstick: update theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider, Flex } from "@chakra-ui/react";
 import "../styles/globals.css";
-import "@fontsource/ibm-plex-sans";
+import "@fontsource/ibm-plex-sans/400.css";
+import theme from "../utils/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
 			<Flex minH="100vh" direction="column" bgColor="000">
 				<Component {...pageProps} />
 			</Flex>
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,28 @@ const Home: NextPage = () => {
 				<title>Arihant</title>
 				<meta property="og:title" content="Arihant" key="title" />
 			</Head>
-			<Box p="12">
-				<Footer />
+			<Box p="12" bg="black" minH="100vh">
+				<Heading>gm!</Heading>
+				<br />
+				<Text>
+					i&apos;m{" "}
+					<CustomLink href="https://explorer.solana.com/address/GsuyNHX76ZGXwisQ2qSyP6nNUgx2DxNtCQXESVswzC6F">
+						Arihant
+					</CustomLink>{" "}
+					- a 20 yr old internet craftsman. just a fancy way to say i build this on the internet.{" "}
+				</Text>
+				<br />
+				<Text>
+					find me on <CustomLink href="https://twitter.com/_arihantbansal">twitter</CustomLink> or{" "}
+					<CustomLink href="https://github.com/arihantbansal">github</CustomLink>.
+				</Text>
+				<br />
+				<Text>
+					currently building products in web3 while trying to graduate with my bachelors in
+					engineering from bits pilani, india.
+				</Text>
+				<br />
+				<Text color="gray">arihantbansal dot sol arihantbansal dot eth</Text>
 			</Box>
 		</>
 	);
